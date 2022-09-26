dfe83973cb0a6431967d51123864a8b79f489eee
YSerhanGENER <gener@email.arizona.edu>
2022-09-26 11:33:54 -0700

    Changed DASH_FFT API call with float complex version

--- lib/fft_impl.cc
+++ lib/fft_impl.cc
@@ -49,31 +49,11 @@ int fft_impl::work(int noutput_items,
 //  	printf("new work called with noutput_items: %d\n", noutput_items);
 
 	while (count++ < noutput_items) {
-	      double *temp_in = new double[2*d_vlen];
-	      double *temp_out = new double[2*d_vlen];
-	      for (size_t i = 0; i < (size_t)d_vlen; i++){
-		const gr_complex x = in[i]; // gr_complex is a type signature for std::complex<float>
-		temp_in[2*i] = (double) x.real();
-		temp_in[2*i+1] = (double) x.imag();
-	      }
-
 	      /* Call out to the DASH_FFT Macro that will hook into CEDR
-	       * complex input and output,  input[2*i+0] = real, input[2*i+1] = imaginary
+	       * complex(float) input and output,
 	       * - d_vlen is size of the complex transform, true is forward, false is inverse */
-	      DASH_FFT(temp_in, temp_out, d_vlen, d_direction);
+	      DASH_FFT_flt((dash_cmplx_flt_type*)in, (dash_cmplx_flt_type*)out, d_vlen, d_direction);
 
-	      // Copy the temporary output buffer into the GR output buffer - and also cast back to float from double
-	      for (size_t i = 0; i < (size_t)d_vlen; i++)
-	      {
-		gr_complex o = gr_complex(temp_out[2*i],temp_out[2*i+1]);
-		*out++ = (gr_complex) o;
-	      }
-
-
-	      // Free the dynamically allocated temp buffers
-	      delete[] temp_in;
-	      delete[] temp_out;
-	      // Move the pointer forward by FFT length
 	      in += d_vlen;
 	      // The out pointer has already been moved forward appropriately by the output copy loop
 	}
